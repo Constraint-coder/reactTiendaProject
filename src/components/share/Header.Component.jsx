702b3dc70dc8f7ba50de5df662e12fbbbe712557
Constraint-coder <mikelalas17@gmail.com>
2026-05-09 14:39:20 -0600

    actualizacion

--- src/components/share/Header.Component.jsx
+++ src/components/share/Header.Component.jsx
@@ -19,7 +19,7 @@ export default function Header() {
            
             
             <div className="flex-shrink-0">
-              <span className="text-2xl font-bold text-indigo-600">EventosApp</span>
+              <span className="text-2xl font-bold text-indigo-600">Super</span>
             </div>
             {/* <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
@@ -36,10 +36,7 @@ export default function Header() {
             </button> */}
             <div className="ml-4 relative">
               <div>
-                <button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                </button>
+
               </div>
             </div>
           </div>
